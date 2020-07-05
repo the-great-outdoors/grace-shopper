fc6dbfa34f07c7650d6fc476d870687e0d9ba01e
Sean Greene <seangreenebrandeis@gmail.com>
2020-07-05 14:04:15 -0400

    added test files

---
+++ tests/index.spec.js
@@ -0,0 +1,12 @@
+require('jest');
+
+const axios = require('axios');
+
+describe('Booleans', ()=>{
+
+    it('tests the testing function', async()=>{
+
+        expect(true===true).toEqual(true);
+    })
+})
+
