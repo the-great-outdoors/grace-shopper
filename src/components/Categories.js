ade40e4a2998d66a7cbf9676fab44322dea701ff
Levi Plant <levijplant@gmail.com>
2020-07-19 20:35:05 -0700

    minor syntax errors fixed

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,65 +1,65 @@
 import React, { useState } from "react";
-import {Grid, Card, Header} from "semantic-ui-react";
+import { Grid, Card, Header, Container } from "semantic-ui-react";
 import './Categories.css';
 import { } from "react-router-dom";
 import axios from "axios";
 import MerchCards from "./Cards";
 
-const Categories=({setMerchandise, merchandise})=>{
+const Categories = ({ setMerchandise, merchandise }) => {
 
-const [displayCats, setDisplayCats] = useState(true)
+    const [displayCats, setDisplayCats] = useState(true)
 
-const handleClick= async(event, data)=>{
-    console.log('you clicked category:', data);
-    const param = data.value;
-    const res = await axios.get(`/api/merchandise/category/${param}`);
-    console.log('returned merchandise?', res.data.data);
-    setMerchandise(res.data.data);
-    setDisplayCats(false);
+    const handleClick = async (event, data) => {
+        console.log('you clicked category:', data);
+        const param = data.value;
+        const res = await axios.get(`/api/merchandise/category/${param}`);
+        console.log('returned merchandise?', res.data.data);
+        setMerchandise(res.data.data);
+        setDisplayCats(false);
 
-}
+    }
 
-    return(
-        displayCats?
-        <Grid>
-            <Grid.Row columns={3}>
-            <Grid.Column>
-                <Card fluid value={3} name='hiking' onClick={handleClick}  className='category' style={{ backgroundImage:`url('/resources/backpack_AZ.jpg')`}}>
-                    <Header className='catHeader' inverted textAlign='center'>Hiking</Header>
-                </Card>
-                </Grid.Column>
-                <Grid.Column>
-                    <Card value={1} fluid style={{ height: '400px', backgroundImage:`url('/resources/climbing_snow.jpg')`, backgroundSize:'cover'}} onClick={handleClick}>
-                    <Header className='catHeader' inverted textAlign='center'>Clothing</Header>
-                    </Card>
-                </Grid.Column>
-                <Grid.Column>
-                    <Card value={5} fluid style={{ height: '400px', backgroundImage:`url('/resources/tents_glow.jpg')`, backgroundSize:'cover'}} onClick={handleClick}>
-                    <Header className='catHeader' inverted textAlign='center'>Camping</Header>
-                    </Card>
-                </Grid.Column>
-                
-            </Grid.Row>
-            <Grid.Row columns={2}>
-                <Grid.Column width={8}>
+    return (
+        displayCats ?
+            <Grid>
+                <Grid.Row columns={3}>
+                    <Grid.Column>
+                        <Card fluid value={3} name='hiking' onClick={handleClick} className='category' style={{ backgroundImage: `url('/resources/backpack_AZ.jpg')` }}>
+                            <Header className='catHeader' inverted textAlign='center'>Hiking</Header>
+                        </Card>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Card value={1} fluid style={{ height: '400px', backgroundImage: `url('/resources/climbing_snow.jpg')`, backgroundSize: 'cover' }} onClick={handleClick}>
+                            <Header className='catHeader' inverted textAlign='center'>Clothing</Header>
+                        </Card>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Card value={5} fluid style={{ height: '400px', backgroundImage: `url('/resources/tents_glow.jpg')`, backgroundSize: 'cover' }} onClick={handleClick}>
+                            <Header className='catHeader' inverted textAlign='center'>Camping</Header>
+                        </Card>
+                    </Grid.Column>
 
-                <Container style={{ height: '400px', backgroundImage:`url('/resources/rock_man_clouds.jpg')`, backgroundSize:'cover'}}>
-                    <Header className='catHeader' inverted textAlign='center'>Climbing</Header>
-                    </Container>
+                </Grid.Row>
+                <Grid.Row columns={2}>
+                    <Grid.Column width={8}>
+
+                        <Container style={{ height: '400px', backgroundImage: `url('/resources/rock_man_clouds.jpg')`, backgroundSize: 'cover' }}>
+                            <Header className='catHeader' inverted textAlign='center'>Climbing</Header>
+                        </Container>
 
 
-                </Grid.Column>
-                <Grid.Column width={8}>
-                <Card value={4} fluid style={{ height: '400px', backgroundImage:`url('/resources/kayak.jpeg')`, backgroundSize:'cover'}} onClick={handleClick}>
-                    <Header className='catHeader' inverted textAlign='center' >Sports</Header>
-                    </Card>
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-        :
-        <Card.Group itemsPerRow={4} style={{ marginTop: '1em' }}>
-            <MerchCards merchandise={merchandise}/>
-        </Card.Group>
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                        <Card value={4} fluid style={{ height: '400px', backgroundImage: `url('/resources/kayak.jpeg')`, backgroundSize: 'cover' }} onClick={handleClick}>
+                            <Header className='catHeader' inverted textAlign='center' >Sports</Header>
+                        </Card>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+            :
+            <Card.Group itemsPerRow={4} style={{ marginTop: '1em' }}>
+                <MerchCards merchandise={merchandise} />
+            </Card.Group>
     )
 }
 
--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Header, Rating, Divider, Button, Input, Breadcrumb} from 'semantic-ui-react';
+import { Grid, Header, Rating, Divider, Button, Input, Breadcrumb } from 'semantic-ui-react';
 import { SideBySideMagnifier } from 'react-image-magnifiers';
 import axios from 'axios';
-import { useParams, useHistory} from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import './ProductPage.css';
 
-const ProductPage = ({item, setItem, cart, setCart}) => {
+const ProductPage = ({ item, setItem, cart, setCart }) => {
 
     const [quantity, setQuantity] = useState(1);
     const [total, setTotal] = useState('');
@@ -13,43 +13,43 @@ const ProductPage = ({item, setItem, cart, setCart}) => {
     const { id } = useParams();
 
     const history = useHistory();
-    const handleClick = (event, data)=>{
-        let path =`/${data.name}`;
+    const handleClick = (event, data) => {
+        let path = `/${data.name}`;
         history.push(path);
     }
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         try {
             axios.get(`/api/merchandise/search/${id}`)
-            .then(res=>{
-                const {data:item} = res;
-                console.log( item.merch);
-                setItem(item.merch);
-            })
+                .then(res => {
+                    const { data: item } = res;
+                    console.log(item.merch);
+                    setItem(item.merch);
+                })
         } catch (error) {
             throw error;
         }
 
     }, []);
 
-    const AddItem= ()=>{
+    const AddItem = () => {
         console.log('clicked addItem')
-       const qty = quantity;
-       const price = item.price;
-        console.log('qty:',qty, 'price:',price);
-       const merch = {
-           merchId:id,
-           quantity:qty,
-           price,
-           name:item.name,
-           description:item.description
+        const qty = quantity;
+        const price = item.price;
+        console.log('qty:', qty, 'price:', price);
+        const merch = {
+            merchId: id,
+            quantity: qty,
+            price,
+            name: item.name,
+            description: item.description
 
-       }
+        }
 
-       const cartArray = cart;
-       cartArray.push(merch);
-       setCart(cartArray);
-       localStorage.setItem('activeCart', JSON.stringify(cartArray));
+        const cartArray = cart;
+        cartArray.push(merch);
+        setCart(cartArray);
+        localStorage.setItem('activeCart', JSON.stringify(cartArray));
     }
 
     const registerChange = (e, data) => {
@@ -59,43 +59,44 @@ const ProductPage = ({item, setItem, cart, setCart}) => {
 
     console.log('items in cart:', cart)
 
-   return(
-    <>   
-       <div style={{ marginTop: '5rem',  }}>
-            <Breadcrumb>
-                <Breadcrumb.Section link name='home' onClick={handleClick}>Home</Breadcrumb.Section>
-                <Breadcrumb.Divider icon='right angle'/>
-                <Breadcrumb.Section link name='categories' onClick={handleClick}>Category</Breadcrumb.Section>
-                <Breadcrumb.Divider icon='right angle'/>
-                <Breadcrumb.Section active>{item.name}</Breadcrumb.Section>
-            </Breadcrumb>
-            <Grid  style={{ marginLeft: '5rem' }} >
-                <Grid.Row columns={2}>
-                    <Grid.Column width={8} style={{ marginRight: '1rem' }}>
-                        <SideBySideMagnifier imageSrc='/resources/backpack_AZ.jpg' imageAlt='Example' alwaysInPlace />
-                    </Grid.Column>
-                    <Grid.Column center width={5} textAlign='right' > 
-                        <Header className='titleblock' as='h1'>{item.name}</Header>
-                        <Header className='titleblock' as='h5' color='grey' >Item #123456</Header>
-                        <Rating className='titleblock' rating={item.rating} maxRating={5}></Rating>
-                        <Header as='h2' color='orange'>${item.price}</Header>
-                        <Header as='h2' style={{fontWeight:'bold'}}>Description</Header><Header as='h3'>{item.description}</Header>
-                        <Input label={{color: 'teal', labelPosition: 'left', content: 'Quantity'}} min={1} max={10} type='number' style={{ marginBottom: '1rem' }} onChange = {registerChange} defaultValue='1' />
-                        <div>
-                            {!addItem?
-                            <Button size='huge'
-                                color='teal'
-                                icon='cart'
-                                content='Add to Cart'
-                                onClick={AddItem} />:
-                                <Button size='huge'
-                            color='grey'
-                            icon='remove'
-                            content='Remove'
-                            onClick={removeItem}/>}
-                            <Button size='huge' basic style={{ marginTop: '1rem', marginLeft: '1rem' }}>Add to wishlist</Button>
-                        </div>
-                    </Grid.Column>
+    return (
+        <>
+            <div style={{ marginTop: '5rem', }}>
+                <Breadcrumb>
+                    <Breadcrumb.Section link name='home' onClick={handleClick}>Home</Breadcrumb.Section>
+                    <Breadcrumb.Divider icon='right angle' />
+                    <Breadcrumb.Section link name='categories' onClick={handleClick}>Category</Breadcrumb.Section>
+                    <Breadcrumb.Divider icon='right angle' />
+                    <Breadcrumb.Section active>{item.name}</Breadcrumb.Section>
+                </Breadcrumb>
+                <Grid style={{ marginLeft: '5rem' }} >
+                    <Grid.Row columns={2}>
+                        <Grid.Column width={8} style={{ marginRight: '1rem' }}>
+                            <SideBySideMagnifier imageSrc='/resources/backpack_AZ.jpg' imageAlt='Example' alwaysInPlace />
+                        </Grid.Column>
+                        <Grid.Column center width={5} textAlign='right' >
+                            <Header className='titleblock' as='h1'>{item.name}</Header>
+                            <Header className='titleblock' as='h5' color='grey' >Item #123456</Header>
+                            <Rating className='titleblock' rating={item.rating} maxRating={5}></Rating>
+                            <Header as='h2' color='orange'>${item.price}</Header>
+                            <Header as='h2' style={{ fontWeight: 'bold' }}>Description</Header><Header as='h3'>{item.description}</Header>
+                            <Input label={{ color: 'teal', labelPosition: 'left', content: 'Quantity' }} min={1} max={10} type='number' style={{ marginBottom: '1rem' }} onChange={registerChange} defaultValue='1' />
+                            <div>
+                                {!addItem ?
+                                    <Button size='huge'
+                                        color='teal'
+                                        icon='cart'
+                                        content='Add to Cart'
+                                        onClick={AddItem} /> :
+                                    <Button size='huge'
+                                        color='grey'
+                                        icon='remove'
+                                        content='Remove'
+                                        onClick={removeItem} />}
+                                <Button size='huge' basic style={{ marginTop: '1rem', marginLeft: '1rem' }}>Add to wishlist</Button>
+                            </div>
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </div>
         </>
@@ -103,43 +104,3 @@ const ProductPage = ({item, setItem, cart, setCart}) => {
 }
 
 export default ProductPage;
-
-// if(!cart){
-//     const res = await axios.post('/api/orders', { 
-//         userId: 1,
-//         status: true,
-//         price,
-//         merchId:item.merch_id,
-//         quantity
-//     })
-//         const newOrder = res.data.orderItem;
-//          console.log('New Order Num:', newOrder);
-//          setCart(newOrder);
-// }
-//     if (cart) {
-//         const res = await axios.post(`/api/orders/`, { 
-//             userId: 1,
-//             price,
-//             merchId:item.merch_id,
-//             quantity,
-//             orderId:cart.orderId});
-//             const newOrderItem = res.data.orderItem;
-//             setCart(newOrderItem);
-//             console.log('New item created:', newOrderItem);
-//         };
-
-//         setAddItem(true);
-//     }
-
-//     const removeItem=async(event, data)=>{
-
-//         try {
-//             const res = await axios.delete(`/api/orders/cart/${cart.item_id}`, {data:{orderId: cart.orderId, item_id:cart.item_id}});
-//             setAddItem(false);
-//             return res;
-
-//         } catch (error) {
-//             throw error; 
-//         }
-        
-//     }
