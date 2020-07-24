556330c377a677c95b12115f3f6a062796ce7eaa
Levi Plant <levijplant@gmail.com>
2020-07-23 21:30:56 -0700

    removed loader from shipping options

--- src/components/ShippingOptions.js
+++ src/components/ShippingOptions.js
@@ -1,25 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Segment, Button, Input, Dropdown } from "semantic-ui-react";
+import { Grid, Segment, Button, Input, Dropdown, Dimmer, Loader, Image } from "semantic-ui-react";
 import { ShippingEdit } from './ShippingEdit';
 import { Shipping } from '../components';
 
-const ShippingOptions = ({ user, setStep}) => {
+const ShippingOptions = ({ user, setStep }) => {
     const [editMode, setEditMode] = useState(false);
-    const[shippingInfo, setShipping] = useState({})
+    const [shippingInfo, setShipping] = useState({});
 
-    useEffect(()=>{
+    // if (!user.user_id) {
+    //     return <div>
+    //         <Segment>
+    //             <Dimmer active inverted>
+    //                 <Loader inverted active>Loading</Loader>
+    //             </Dimmer>
+
+    //             <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+    //         </Segment>
+    //     </div>;
+    // };
+
+    useEffect(() => {
         if (user.user_id) {
             console.log('setting setShipping');
             const { firstname, lastname, userPreferences: { street, city, state, zip, shipping } } = user;
-    
+
             setShipping({ firstname, lastname, street, city, state, zip, shipping });
-        }else{
+        } else {
             console.log('guest user');
         }
 
     }, []);
-  
-   
+
+
     return (
         <Grid>
             {editMode ?
